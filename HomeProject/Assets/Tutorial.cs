ad82d0b39de4ce2eb70b59218c29469e37b3f168
Tanner <thebow25@gmail.com>
2019-08-30 22:42:13 -0600

    Tutorial#1 Project To change Colors of Sphere

---
+++ HomeProject/Assets/Tutorial.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    private Renderer _renderer;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            _renderer.material.color = Color.green;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _renderer.material.color = Color.magenta;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            _renderer.material.color = Color.blue;
+        }
+
+  
+
+
+    }
+}
