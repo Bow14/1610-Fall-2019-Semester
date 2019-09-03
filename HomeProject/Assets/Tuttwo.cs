694a1486503ce1c95743a1a42b7b1dbffb921d72
Tanner <thebow25@gmail.com>
2019-09-02 22:46:35 -0600

    Tutorial#2 Functions

---
+++ HomeProject/Assets/Tuttwo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuttwo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int MyGoodInt = 10;
+        MyGoodInt = 45;
+            Debug.Log(MyGoodInt * 4);
+            {
+                MyGoodInt = MultiplyBySix(MyGoodInt);
+                Debug.Log(MyGoodInt);
+            }
+            int MultiplyBySix(int number)
+            {
+                int ret;
+                ret = number * 6;
+                return ret;
+            }
+
+    }
+
+    // Update is called once per frame
+}
