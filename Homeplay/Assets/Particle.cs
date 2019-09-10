121e4060043a0d13c1cabf7add2a9e76c6145a37
Tanner <thebow25@gmail.com>
2019-09-09 22:47:13 -0600

    Work in Progress experiment#2

---
+++ Homeplay/Assets/Particle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.UIElements;
+
+
+public class Particle : MonoBehaviour
+{
+    public float floathigh = 7.5f;
+
+    public int intvaule = 50;
+    public string stringvaule = "Bow";
+
+    public int firepower;
+    public UnityEvent Event;
+
+//    private void OnParticleTrigger()
+//    {
+//        ParticleSystem.Destroy(); 
+//    }
+
+//    private void OnMouseDown()
+//    {
+//        MouseButton leftMouse;
+//        leftMouse = MouseButton.LeftMouse;
+//        Debug.Log("I am clicking with it");
+//    }
+    
+
+// i have been sick for the past 4 days and im feeling not too well now and i wanted to do my commit for today but i am completely lost on how to make the mouse work
+//or how to make the particle system to even pop up but im gonna give it a break for tonight sorry for this worthless commit
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
