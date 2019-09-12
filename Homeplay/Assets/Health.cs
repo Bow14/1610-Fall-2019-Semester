931ffe0c51e64bd2ab25e4da02c524dbce00391b
Tanner <thebow25@gmail.com>
2019-09-12 13:11:33 -0600

    Power Up/Particle Event

---
+++ Homeplay/Assets/Health.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+	public float health=5;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<PowerupHolder>())
+		{
+			health+= other.GetComponent<PowerupHolder>().Powerup.HealthAmount;
+			print(health);
+		}
+	}
+}
---
+++ Homeplay/Assets/OnMouseClick.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnMouseClick : MonoBehaviour
+{
+
+	public UnityEvent Click; 
+	private void OnMouseDown()
+	{
+		Click.Invoke();
+	}
+}
