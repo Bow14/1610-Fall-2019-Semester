7ddd1d1b7444c7a4a2ed719907f5b070420c57ef
Tanner <thebow25@gmail.com>
2019-09-14 23:01:21 -0600

    Importing/Tutorial Code#3

---
+++ HomeProject/Assets/Bug.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bug : MonoBehaviour {
+
+	// Use this for initialization
+	// This is a unity tutorial #3
+	//And using this experience for future coding experiments 
+	
+	//Code doesn't work because of import so i will figure that problem out soon
+	
+//	void Start () 
+//	{
+//		Debug.Log(transform.position.x);
+//		if (transform.position.y <= 8f) ;
+//		{
+//			Debug.Log("Im going to hit the ground!");
+//		}
+
+	}
+	
+
