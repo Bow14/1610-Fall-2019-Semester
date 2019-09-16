72d99bd8104f343d1c5517eb45f21ed6fc9980bf
Tanner <thebow25@gmail.com>
2019-09-15 22:22:49 -0600

    Import Stuff i believe

--- HomeProject/Assets/Bug.cs
+++ HomeProject/Assets/Bug.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bug : MonoBehaviour {
+public class Bug : MonoBehaviour
+{
 
 	// Use this for initialization
 	// This is a unity tutorial #3
 	//And using this experience for future coding experiments 
+
 	
-	//Code doesn't work because of import so i will figure that problem out soon
-	
-//	void Start () 
-//	{
-//		Debug.Log(transform.position.x);
-//		if (transform.position.y <= 8f) ;
-//		{
-//			Debug.Log("Im going to hit the ground!");
-//		}
+
+	void Start()
+	{
+		Debug.Log(transform.position.x);
+		if (transform.position.y <= 8f) ;
+		{
+			Debug.Log("Im going to hit the ground!");
+		}
 
 	}
-	
+}	
 
---
+++ Homeplay/Assets/Sytutorial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sytutorial : MonoBehaviour {
+
+	
+	// Use this for initialization
+	//unity tutorial code will probably use for experimental code and slightly fixed import code problem
+	
+	void Start () 
+	{
+		Debug.Log(transform.position.x);
+		if (transform.position.y <= 5f)
+		{
+			Debug.Log("Im about to fall!");
+		}
+		
+	}
+	
+	// Update is called once per frame
+}
