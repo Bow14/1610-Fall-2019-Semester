664ba4ec4d68d311212bc9557bcf8b6dbb41f74f
Tanner <thebow25@gmail.com>
2019-09-15 22:57:40 -0600

    Working on new tutorial Code

---
+++ Homeplay/Assets/IfTuto.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Theif : MonoBehaviour {
+	
+	// Update is called once per frame
+	//unity tutorial to understand if statements to use in experimental code
+	
+	//Work in progress
+/*	private float coffeeTemperature = 85.0f;
+	private float hotlimitTemperature = 70.0f;
+	private float coldLimitTemperature = 40.0f;
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+		coffeeTemperature -= Time.deltaTime * 5f;
+
+
+	}*/
+
+//	private void TemperatureTest()
+//	{
+//		throw new System.NotImplementedException();
+//	}
+}
