123664c1f841d5c56faa3f0ad7310cd7bef5ba71
Tanner <thebow25@gmail.com>
2019-09-17 15:44:42 -0600

    Jump code

---
+++ Homeplay/Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿ 
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+
+	public float speed  = 1f;
+	private Vector3 position;
+	public CharacterController controller;
+	public float gravity = 3f;
+
+	public float jumpspeed = 20f;
+	void Update()
+	{
+		controller.Move(position *= Time.deltaTime);
+		if (Input.GetButtonDown("Jump"))
+		{
+			position.y = jumpspeed;		
+		}
+
+		if (!controller.isGrounded)
+		{
+			position.y -= gravity;
+		}	
+		position.x = speed* Input.GetAxis("Horizontal");;
+	}
+}
