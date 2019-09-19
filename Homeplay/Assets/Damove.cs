97c741d910208b52b122fb13624ba09b4bb79bc4
Tanner <thebow25@gmail.com>
2019-09-19 15:47:20 -0600

    Added DoubleJump/Isgrounded

--- Homeplay/Assets/Damove.cs
+++ Homeplay/Assets/Damove.cs
@@ -2,19 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(CharacterController))] //makes it where you need a charactercontroller for this script
 public class Damove : MonoBehaviour
 
 {
+	private Vector3 position;
 	private CharacterController controller;
 	public float moveSpeed = 10f;
 	public float gravity = 3f;
 	public float jumpSpeed = 30f;
-	private Vector3 position;
+	private int jumpCount;
+	public int jumpCountMax = 2;
+	
 
 	public void Start()
 	{
-		controller = GetComponent<CharacterController>();
+		controller = GetComponent<CharacterController>(); //grabs charactercontroller when you add this script
 	}
 
 	void Update ()
@@ -22,8 +25,15 @@ public class Damove : MonoBehaviour
 		position.x = moveSpeed*Input.GetAxis("Horizontal");
 		position.y -= gravity;
 
-		if (Input.GetButtonDown("Jump"))
+		if (controller.isGrounded)
 		{
+			position.y = 0;
+			jumpCount = 0; //makes it where you can do multiple double jumps in one game
+		}
+
+		if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax) //the restart modify
+		{
+			jumpCount++; //plus plus = 1
 			position.y = jumpSpeed;
 		}
 		controller.Move(position * Time.deltaTime);
