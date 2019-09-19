831d11bf0add2fd3d1fd7e19dee4365c8cff8f15
Tanner <thebow25@gmail.com>
2019-09-19 15:33:11 -0600

    Smooth Revised JumpCode with private controller code

---
+++ Homeplay/Assets/Damove.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class Damove : MonoBehaviour
+
+{
+	private CharacterController controller;
+	public float moveSpeed = 10f;
+	public float gravity = 3f;
+	public float jumpSpeed = 30f;
+	private Vector3 position;
+
+	public void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update ()
+	{
+		position.x = moveSpeed*Input.GetAxis("Horizontal");
+		position.y -= gravity;
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			position.y = jumpSpeed;
+		}
+		controller.Move(position * Time.deltaTime);
+
+	}
+}
