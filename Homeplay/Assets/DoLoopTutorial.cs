0d2312b6f5b2bd147e3e3a74752c5a103b8f93eb
Tanner <thebow25@gmail.com>
2019-09-20 23:31:07 -0600

    Loop Tutorial Code

---
+++ Homeplay/Assets/DoLoopTutorial.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	//This is Do loop tutorial from unity hopefully will help for later experiments
+	// Use this for initialization
+	
+	
+	void Start () 
+	{
+		bool Continue = false;
+        
+		do
+		{
+			print ("Once upon the time was not a hate message");
+            
+		}while(Continue == true);
+		
+		
+	}
+	
+	// Update is called once per frame
+}
---
+++ Homeplay/Assets/WhileLoopTutoiral.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoopTutoiral : MonoBehaviour {
+	
+	//This is the unity tutorial module for loops so i can gain a little more understanding to use for my games and such
+	//sorry that im doing a bunch of tutorials just trying to get comfortable before heavy experimenting
+
+	public int bearshunted = 4;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		while (bearshunted > 0)
+		{
+			 Debug.Log("Bear Has been killed");
+			 bearshunted--;
+		}
+
+	}
+	
+	
+}
