4feeac0d218167fca61b9e5af96cd94142e787de
Tanner <thebow25@gmail.com>
2019-09-23 19:12:12 -0600

    Put a enable gravity to my fall cube for the player

---
+++ Homeplay/Assets/gravityDisabled.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravityDisabled : MonoBehaviour {
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other == true);
+		{
+			GetComponent<Rigidbody>().useGravity = true;
+		}
+		
+	}
+}
