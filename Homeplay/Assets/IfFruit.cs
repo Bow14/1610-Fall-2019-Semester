7ad4f8a888f6f16dc4127fa9a0121be99b38a103
Tanner <thebow25@gmail.com>
2019-09-24 15:24:35 -0600

    IfFruitRight

---
+++ Homeplay/Assets/IfFruit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfFruit : MonoBehaviour
+{
+
+	public bool appleirsRipe, orangeIsRipe, peachIsRipe;
+	// Use this for initialization
+	void Start () 
+	{
+		if (appleirsRipe)
+		{
+			print("Go Pick the Apple");
+			
+		}
+		else if (orangeIsRipe)
+		{
+			print("Go Pick the Orange");
+		}
+		else if (peachIsRipe)
+		{
+		print("go pick the peach");
+		
+		}
+		else 
+		{
+			print("Nothing is good");	
+		}
+		
+		
+		
+
+		}
+	
+	// Update is called once per frame
+}
---
+++ Homeplay/Assets/statementif.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class statementif : MonoBehaviour {
+
+	// Use this for initialization
+	public int a = 4;
+	public int b = 5;
+	public int c = 10;
+
+	public bool lightsOn = false;
+	public string password = "Friend";
+	void Start () 
+	{
+		if (a+b != c)
+		{
+			print(true);
+			
+		}
+		else
+		{
+			print(false);
+		}
+
+		if (password != "OU812")
+		{
+			print(true);
+		}
+
+		if (!lightsOn)
+		{
+		print(true);
+			
+		}
+		
+	}
+	
+	// Update is called once per frame
+}
