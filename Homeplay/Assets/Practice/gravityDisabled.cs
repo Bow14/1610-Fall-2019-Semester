adf283b9017b61b27c17f1f5d178c4dede6a96c1
Tanner <thebow25@gmail.com>
2019-09-26 13:54:13 -0600

    BouncyCode/Fixed Fall Code

---
+++ Homeplay/Assets/Practice/gravityDisabled.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravityDisabled : MonoBehaviour
+{
+
+	public float time;
+	private void OnTriggerEnter(Collider other)
+	{
+		StartCoroutine(Wait());
+	}
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(time);
+		GetComponent<Rigidbody>().useGravity = true;
+	}
+	
+	
+}
---
+++ Homeplay/Assets/Rubber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rubber : MonoBehaviour
+{
+	public Vector3 bouncy;
+
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Debug.Log("Bounce");
+		rb.AddForce(bouncy);
+	}
+}
