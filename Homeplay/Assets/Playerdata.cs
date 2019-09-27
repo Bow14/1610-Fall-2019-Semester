76cd505338343302067804536ac970fb9ac8227e
Tanner <thebow25@gmail.com>
2019-09-27 13:12:34 -0600

    Added PlayerDate Stats

---
+++ Homeplay/Assets/Playerdata.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerdata : ScriptableObject
+{
+
+
+        public float Health = 10f;
+        public float Stamina = 5f;
+        public float Armor = 2f;
+        public float Color;
+        public float Sanity = 5f;
+        public float Speed = 2f;
+        public float DeathWish = 1f; //Releses his/her true being
+        public float Clover = 2f; // Luck pretty much
+        
+}
