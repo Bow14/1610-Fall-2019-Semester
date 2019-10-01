b0d2b526b3b3ac2aec97afabc7d576beb78b27d6
Tanner <thebow25@gmail.com>
2019-09-30 18:31:54 -0600

    Scopes And mod Tutorial Unity

---
+++ Homeplay/Assets/AnotherClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherClass : MonoBehaviour
+{
+	//This is a unity tutorial with scopes and modifiers 
+	public int apples;
+
+	public int bananas;
+
+	private int stapler;
+
+	private int sellotape;
+
+	public void FruitMachine(int a, int b)
+	{
+		int answer;
+		answer = a = b;
+		Debug.Log("Frut total:" + answer);
+	}
+
+	private void OfficeSort(int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Office Supplies Total:" + answer);
+	}
+}
---
+++ Homeplay/Assets/Scopes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scopes : MonoBehaviour
+{
+
+	//This is a unity tutorial about scopes and modifers learning to extract stuff from other things i believe 
+	
+	public int alpha;
+
+	private int beta = 0;
+	private int  gamma =  5;
+
+
+	private AnotherClass myOtherClass;
+
+	// Use this for initialization
+	void Start ()
+	{
+		alpha = 29;
+		myOtherClass = new AnotherClass();
+		myOtherClass.FruitMachine(alpha, myOtherClass.apples);
+
+	}
+
+	void Example(int pens, int crayons)
+	{
+		int answer;
+		answer = pens * crayons * alpha;
+		Debug.Log(answer);
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log("Alpha is set to:" + alpha);
+		
+	}
+}
