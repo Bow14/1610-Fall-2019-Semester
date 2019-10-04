2436d611ed93c97f5144ff4c02a980cb5841e4b1
Tanner <thebow25@gmail.com>
2019-10-04 14:24:39 -0600

    Added Ouch Script for a funny moment and memorization

---
+++ Homeplay/Assets/Ouchi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Ouchi : MonoBehaviour
+{
+	public UnityEvent Event;
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.collider)
+		{
+			Event.Invoke();
+			Debug.Log("OOOOOOOOOAAHHHHHHH");
+		}
+	}
+}
