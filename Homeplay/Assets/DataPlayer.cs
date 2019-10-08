4acf013b1b9d07a3f868e38c33bbbe740bb6e598
Tanner <thebow25@gmail.com>
2019-10-08 16:00:15 -0600

    Edited scriptital objects

---
+++ Homeplay/Assets/DataPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPlayer : GameArtData
+{
+
+	
+	public WeaponData weapon;
+
+	public void InstancePlayer()
+	{
+		var  newPlayer = Instantiate(prefab);
+		var newSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+		newSprite.color = color;
+		newSprite.sprite = sprite;
+	}
+}
---
+++ Homeplay/Assets/WeaponData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CreateAssetMenu]
+public class WeaponData : GameArtData
+{
+
+    public int weaponPower = 10;
+
+}
