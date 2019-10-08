973e9cf9bdcfa4159885576625bfa61b0ab1aba7
Tanner <thebow25@gmail.com>
2019-10-07 21:17:26 -0600

    Unity Tutorial UpdateFixedUpdate

---
+++ Homeplay/Assets/fixedUpTutorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fixedUpTutorial : MonoBehaviour
+{
+    //This is a unity tutorial update and fixed update from the module page
+
+    // Update is called once per frame
+    private float fixedUpdateTimer;
+    private float UpdateTimer;
+    void FixedUpdate()
+    {
+        Debug.Log("I am fixing his mistakes : "+ Time.deltaTime);
+    }
+    
+    
+    void Update()
+    {
+        Debug.Log("Good Ol Update : "+ Time.deltaTime);
+    }
+}
