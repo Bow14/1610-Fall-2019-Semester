3ef396e073247ef6eb11ebcc690eae49ccaa4410
Tanner <thebow25@gmail.com>
2019-10-21 22:42:37 -0600

    Rewrote a if script from memory

---
+++ Homeplay/Assets/Scripts/Practice/RemadeIF.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemadeIF : MonoBehaviour
+{
+
+	public bool mrbrightsideplaying, delusionplaying, sickflowplaying;
+	// Use this for initialization
+	void Start () {
+		if (delusionplaying)
+		{
+			print("Hell ya *Headbangs*");
+		}
+		else if (mrbrightsideplaying)
+		{
+			print("Aww shit *Cries*");
+		}
+		else if (sickflowplaying)
+		{
+			print("Hmmmm *starts to fill with anger and pain");
+		}
+		else
+		{
+			print("Existence is not real");
+		}
+		
+		}
+	
+	
+}
