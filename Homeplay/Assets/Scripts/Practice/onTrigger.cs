8339e013fbc105f14128567e06f1dd1d2ca487a4
Tanner <thebow25@gmail.com>
2019-10-22 15:43:57 -0600

    Added GameArtCollection

--- Homeplay/Assets/Scripts/Practice/onTrigger.cs
+++ Homeplay/Assets/Scripts/Practice/onTrigger.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
+[RequireComponent(typeof(Collider))]
 public class onTrigger : MonoBehaviour
 {
 
     public UnityEvent triggerEvent;
+
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
