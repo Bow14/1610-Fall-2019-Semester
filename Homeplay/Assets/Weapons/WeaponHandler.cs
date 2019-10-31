858567010dc8ae51cf837acc77f07cceefb5dea3
Tanner <thebow25@gmail.com>
2019-10-30 23:57:11 -0600

    Working on weaponSwitch code

---
+++ Homeplay/Assets/Weapons/WeaponHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+
+public class WeaponHandler : ScriptableObject {
+
+
+	//public void WeaponSwitch(WeaponData data)
+	//{
+	//	weaponDataObj = data;
+	//}
+	//public void WeaponData weaponDataObj;
+	
+	public void onActivate()
+	{
+		
+	}
+}
