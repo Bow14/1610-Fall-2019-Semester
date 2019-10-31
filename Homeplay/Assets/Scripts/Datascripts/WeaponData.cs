e2fa5331ff27d73e7aab300ed82f0080bcad6382
Tanner <thebow25@gmail.com>
2019-10-31 00:50:11 -0600

    MadeWeaponCodeWork/Now to activate it and such

--- Homeplay/Assets/Scripts/Datascripts/WeaponData.cs
+++ Homeplay/Assets/Scripts/Datascripts/WeaponData.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
+
 [CreateAssetMenu]
 public class WeaponData : GameArtData
 {
 
     public int weaponPower = 10;
-    
+    public UnityEvent OnActivate;
+
+    public void Activate()
+    {
+        OnActivate.Invoke();
+    }
 
 }
