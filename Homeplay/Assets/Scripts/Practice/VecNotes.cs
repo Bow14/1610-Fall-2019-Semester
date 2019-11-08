c22a70dd081968552343a697042b39536cc34e9c
Tanner <thebow25@gmail.com>
2019-11-07 22:54:52 -0700

    Added Notes about Vectors/Vector MAth and dots and crosses

---
+++ Homeplay/Assets/Scripts/Practice/VecNotes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VecNotes : MonoBehaviour {
+
+	// Use this for initialization
+	
+	
+	//Im watching a youtube tutorial about vector math so ill be taking notes here
+	//Using 2d vectors with graphing points and such are useful when you need to have the game make predictions
+	
+	//Unity runs on a left hand system where you put your finger into a l shape and it will repersent z,y,x in that order
+	//A Dot Product--Produces a scaler for graphing on the xyz axies = to find this we need to multiply their values 
+	//and get their sum
+	// unity has a built in calaculator for dot products Vector3.Dot(Vector A, Vector B)
+	
+	//Use Cross products to make a whole new vector = A ^ B = C have to use cross multiplication with xyz
+	//Vector3.Cross(Vector A, Vector B) this is what you need to do to get unity to calculate for you
+	
+
+	// Update is called once per frame
+}
