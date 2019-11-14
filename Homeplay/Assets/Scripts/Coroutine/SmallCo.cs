9bd5bc2827e026fbba921b92dd92d521d4d5f0d8
Tanner <thebow25@gmail.com>
2019-11-14 15:43:22 -0700

    Redid Coroutine!

---
+++ Homeplay/Assets/Scripts/Coroutine/SmallCo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallCo : MonoBehaviour
+{
+	public bool canRun = true;
+	public intData index;
+	public WaitForSeconds wfsobj;
+	public float seconds = 1f;
+	
+
+	public void Run()
+	{
+		StartCoroutine(onRun());
+		wfsobj = new WaitForSeconds(seconds);
+	}
+
+	IEnumerator onRun() {
+		while (index.value > 0)
+		{
+			index.value--;
+			yield return new WaitForSeconds(1f);
+			
+		}
+		
+	}
+	
+}
