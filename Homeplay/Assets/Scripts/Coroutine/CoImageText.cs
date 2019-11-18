e2fbe5c7d3e4e33dd37bf710df3424f96da6c87f
Tanner <thebow25@gmail.com>
2019-11-18 00:29:04 -0700

    Added it where it will show up in the ui countdown

--- Homeplay/Assets/Scripts/Coroutine/CoImageText.cs
+++ Homeplay/Assets/Scripts/Coroutine/CoImageText.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Net.Mime;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Text))]
 public class CoImageText : MonoBehaviour {
 	
 	private Text countText;
+	public UnityEvent down;
+
+	private void Update()
+	{
+		down.Invoke();
+	}
 
 	public void UpdateText(intData data)
 	{
