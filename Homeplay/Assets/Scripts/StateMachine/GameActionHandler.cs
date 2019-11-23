e7b173efd86b84ad2794e4898218c8fe30c66db3
Tanner <thebow25@gmail.com>
2019-11-23 13:37:42 -0700

    GetAction GeThandler class from thursday

---
+++ Homeplay/Assets/Scripts/StateMachine/GameActionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+
+	public GameAction gameActionObj;
+	public  UnityEvent  handlerEvent;
+
+	private void Start()
+	{
+		gameActionObj.action = RaiseHandler;
+	}
+
+	public void RaiseHandler()
+	{
+		handlerEvent.Invoke();
+	}
+	
+	// this is all code from your lesson i got off your github
+	
+}
