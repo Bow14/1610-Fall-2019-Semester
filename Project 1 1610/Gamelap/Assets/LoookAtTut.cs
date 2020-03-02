44ced7e47c96eab2cebbdf6d0418c7eb740e55e5
Tanner <thebow25@gmail.com>
2020-03-02 13:07:19 -0700

    Trying to easily get back my coding habits with a tutorial

---
+++ Project 1 1610/Gamelap/Assets/LoookAtTut.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoookAtTut : MonoBehaviour
+{
+
+	public Transform sphere;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt(sphere); //this is a unity tutorial that is used to instantly look at an object oor a transform
+		
+	}
+}
