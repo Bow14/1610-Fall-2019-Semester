96256604b8f7bb406561fe4c17e06ed246270b78
Tanner <thebow25@gmail.com>
2020-09-09 23:10:21 -0600

    Just made a very small script for memory.

---
+++ Homeplay/Assets/Scripts/Practice/Greetings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Greetings : MonoBehaviour
+{
+	private UnityEvent damn;
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			print("Damn son where did yooo find this");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
